040ec55a60aa9554690ed617d88f6e2c86277404
Andreas Ciliox <126715885+aclx1@users.noreply.github.com>
2026-02-03 12:38:39 +0100

    Add additional detection for MANUFACTURER_QDS
    Added newer Qwater 5.5

--- components/wmbus/driver_qwater.cpp
+++ components/wmbus/driver_qwater.cpp
@@ -45,7 +45,12 @@ namespace
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x16);
         di.addDetection(MANUFACTURER_QDS, 0x06,  0x18);
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x18);
+        di.addDetection(MANUFACTURER_QDS, 0x06,  0x19);
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x19);
+        di.addDetection(MANUFACTURER_QDS, 0x06,  0x1a);
+        di.addDetection(MANUFACTURER_QDS, 0x07,  0x1a);
+        di.addDetection(MANUFACTURER_QDS, 0x06,  0x1d);
+        di.addDetection(MANUFACTURER_QDS, 0x07,  0x1d);
         di.addDetection(MANUFACTURER_QDS, 0x06,  0x35);
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x35);
         di.usesProcessContent();
