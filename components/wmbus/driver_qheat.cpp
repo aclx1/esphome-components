ac0f3d1c1a46dc4633e80e6248b18b2dbefbee93
Andreas Ciliox <126715885+aclx1@users.noreply.github.com>
2026-02-03 12:44:26 +0100

    Add detection for new manufacturer code 0x48
    Add Qheat 5.5 2026

--- components/wmbus/driver_qheat.cpp
+++ components/wmbus/driver_qheat.cpp
@@ -36,6 +36,7 @@ namespace
         di.addLinkMode(LinkMode::C1);
         di.addDetection(MANUFACTURER_QDS, 0x04,  0x23);
         di.addDetection(MANUFACTURER_QDS, 0x04,  0x46);
+        di.addDetection(MANUFACTURER_QDS, 0x04,  0x48);
         di.addDetection(MANUFACTURER_QDS, 0x37,  0x23);
         di.usesProcessContent();
         di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new Driver(mi, di)); });
